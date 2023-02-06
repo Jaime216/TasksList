362318f7459cc9fdefca25837450630aed9bf88f
Jaime216 <113053801+Jaime216@users.noreply.github.com>
2023-02-06 12:41:38 +0000

    s

--- src/App.jsx
+++ src/App.jsx
@@ -20,10 +20,7 @@ function App() {
       texto: input,
       completada: false
     };
-    if (nuevaTarea.includes("pene")){
-      alert("eee pillin")
-      return
-    }
+    
     setTareas([...tareas,nuevaTarea]);
     let tareasAct = [...tareas,nuevaTarea];
     window.localStorage.setItem('Tareas',JSON.stringify(tareasAct))
